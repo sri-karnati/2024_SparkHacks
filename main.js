7d2bd3fb87994efc7bdaddc37d6c836650c33ec6
shri124 <spate615@uic.edu>
2024-02-10 15:27:33 -0600

    added changes to webpages

--- main.js
+++ main.js
@@ -1,10 +1,6 @@
 // Ensure this is the correct path to your Python executable
 const command = `"C:\\Users\\sohum\\OneDrive\\Documents\\SparkHacks2024\\dist\\retreive.exe"`;
 
-
-
-
-// Retrieve data from Python side
 exec(command, (error, stdout, stderr) => {
     if (error) {
         console.error(`exec error: ${error}`);
@@ -29,3 +25,27 @@ exec(command, (error, stdout, stderr) => {
         console.log('No output received.');
     }
 });
+
+
+function sendToPython() {
+    const { exec } = require('child_process');
+
+// Example string to pass to the Python script
+    var crop = document.getElementById("dropdown1").value;
+    var state = document.getElementById("dropdown2").value;
+    const inputString = crop + "." + state;
+
+    exec(`python retreive.py "${inputString}"`, (error, stdout, stderr) => {
+        if (error) {
+            console.error(`exec error: ${error}`);
+            return;
+        }
+        // Output the returned string from Python
+        console.log(`Returned string: ${stdout}`);
+        if (stderr) {
+            console.error(`stderr: ${stderr}`);
+        }
+});
+}
+
+// Retrieve data from Python side
